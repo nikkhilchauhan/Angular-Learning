3aa71cd07eeaadc4e9d123d4aeffde86de5a246b
Nikhil Chauhan <nikhil@Nikhils-MacBook-Pro.local>
2020-05-15 23:33:23 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
 
+interface TODO {
+  id: number;
+  todo: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'my-first-app';
+  inputTodo = '';
+  todoArray: TODO[];
+  flagTodo: boolean = false;
+
+  ngOnInit() {
+    this.todoArray = [];
+    this.inputTodo = '';
+  }
+
+  inputHandler(event: any) {
+    this.inputTodo = event.target.value;
+  }
+
+  addButtonPressed() {
+    if (this.inputTodo !== '') {
+      const newItem: TODO = {
+        id: Date.now(),
+        todo: this.inputTodo,
+      };
+      console.log(newItem);
+      this.todoArray.push(newItem);
+      this.flagTodo = false;
+      this.inputTodo = '';
+      console.log(this.todoArray);
+    } else {
+      this.flagTodo = true;
+    }
+  }
+
+  deleteTodo(id) {
+    this.todoArray = this.todoArray.filter((todo) => {
+      return todo.id !== id;
+    });
+  }
 }
